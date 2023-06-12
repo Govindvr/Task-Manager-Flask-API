28cbb307334837ff6f96c4f2236ea8759f33cac7
Govind V R <govindvr02@gmail.com>
2023-06-12 20:01:12 +0530

    FIX: Migrations Added

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,17 @@ from flask_restful import Api
 from config import Config
 from .models import db
 from .routes import initialise_routes
+from flask_migrate import Migrate
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     # Initialize extensions
     db.init_app(app)
+
+    # Initialize flask-migrate
+    migrate = Migrate(app, db)
 
     # Register API resources
     api = Api(app)
