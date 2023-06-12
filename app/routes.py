21b515c8a9f1f22e2d04040f9d3dd17fadb037cf
Govind V R <govindvr02@gmail.com>
2023-06-12 20:17:51 +0530

    FIX: Endpoints Updated

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from .resources import TaskResource, TasksResource, TaskStatusResource
 
 def initialise_routes(api):
-    api.add_resource(TaskResource, '/tasks/<int:task_id>')
-    api.add_resource(TasksResource, '/tasks')
-    api.add_resource(TaskStatusResource, '/tasks/<int:task_id>/status')
+    api.add_resource(TaskResource, '/api/tasks/<int:task_id>')
+    api.add_resource(TasksResource, '/api/tasks')
+    api.add_resource(TaskStatusResource, '/api/tasks/<int:task_id>/status')
