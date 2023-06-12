0442063a39ba212b2facd668fb3ade387b0ed0c9
Govind V R <govindvr02@gmail.com>
2023-06-12 20:14:34 +0530

    FIX: Fix Issue with PUT and GET on Individual Task

--- app/resources.py
+++ app/resources.py
@@ -9,12 +9,14 @@ parser.add_argument('description', type=str)
 parser.add_argument('due_date', type=str, help='Due date must be in YYYY-MM-DD format')
 
 status_parser = reqparse.RequestParser()
-status_parser.add_argument('status', type=str, required=True, choices=["Incomplete", "Completed", "In Progress"], help='Status is required')
+status_parser.add_argument('status', type=str, required=True, 
+                           choices=["Incomplete", "Completed", "In Progress"], 
+                           help='Status is required and Should be one of Incomplete, Completed, In Progress')
 
 
 class TasksResource(Resource):
     def get(self):
-        tasks = Task.query.all()
+        tasks = Task.query.all().order_by(Task.created_at.desc())
         return jsonify([task.to_dict() for task in tasks])
     
     def post(self):
@@ -33,15 +35,20 @@ class TasksResource(Resource):
 
 class TaskResource(Resource):
     def get(self, task_id):
+        task = Task.query.filter_by(id=task_id).first()
+        return jsonify(task.to_dict())
+    
+    def put(self, task_id):
         args = parser.parse_args()
         task = Task.query.filter_by(id=task_id).first()
         task.title = args['title']
         task.description = args['description']
         task.due_date = datetime.strptime(args['due_date'], '%Y-%m-%d')
-        return jsonify(task.to_dict())
-    
-    def put(self, task_id):
-        task = Task.query.filter_by(id=task_id).first()
+        db.session.commit()
+        return jsonify({
+            "message": "Task updated successfully",
+            "task": task.to_dict()
+            })
     
     def delete(self, task_id):
         task = Task.query.filter_by(id=task_id).first()
