4a084b83ccc6a30dbbaa7edf456ab50951f0b28e
Govind V R <govindvr02@gmail.com>
2023-06-12 19:59:46 +0530

    FIX: Database URI Issue

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
